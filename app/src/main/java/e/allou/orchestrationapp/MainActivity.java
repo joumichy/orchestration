202ffa0594925b517d40f82d2969f3978bb90714
john <allou.john@hotmail.fr>
2020-02-14 01:00:50 +0100

    second commit

---
+++ app/src/main/java/e/allou/orchestrationapp/MainActivity.java
@@ -0,0 +1,22 @@
+package e.allou.orchestrationapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void message(View view ){
+        Toast.makeText(this, "Salut la compagnie !", Toast.LENGTH_SHORT).show();
+    }
+}
